3b8ef44c73bfd77d8ba0bf69710b2b478797c4a8
oribenartzyM <121542751+oribenartzyM@users.noreply.github.com>
2023-02-28 10:33:21 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "subscription_pricing_id" {
   description = "The subscription pricing ID"
   value       = { for plan, pricing in azurerm_security_center_subscription_pricing.asc_plans : plan => pricing.id }
 }
+  
+output "plans_and_subplans" {
+  description = "The plan subplan"
+  value       = azurerm_security_center_subscription_pricing.asc_plans
+}
