2ce0c6647ce32076eeff62fd549cb981eca7d088
oribenartzy <oribenartzy@microsoft.com>
2023-03-05 12:28:43 +0200

    Limiting the subplans argument to Servers plan only

--- main.tf
+++ main.tf
@@ -1,10 +1,14 @@
 locals {
   final_plans_list = contains(var.mdc_plans_list,"Databases") ? setsubtract(setunion(var.mdc_plans_list, var.mdc_databases_plans), ["Databases"]) : var.mdc_plans_list
+  updated_subplans = merge(
+    { for k, v in var.subplans : k => (k == "VirtualMachines" ? v : null) }
+  )
+
 }
 
 resource "azurerm_security_center_subscription_pricing" "asc_plans" {
   for_each = local.final_plans_list
   tier          = lookup(var.statuses, each.key, var.default_status) ? "Standard" : "Free"
   resource_type = each.value
-  subplan       = lookup(var.statuses, each.key, var.default_status) ? lookup(var.subplans, each.key, each.key == "StorageAccounts" ? "PerStorageAccount" : var.default_subplan) : null
+  subplan       = lookup(var.statuses, each.key, var.default_status) ? lookup(local.updated_subplans, each.key, each.key == "StorageAccounts" ? "PerStorageAccount" : var.default_subplan) : null
 }
