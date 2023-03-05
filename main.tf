2ae2eb61c979b307f949acb721a982eddd3cd037
oribenartzy <oribenartzy@microsoft.com>
2023-03-05 10:37:53 +0200

    Clean subplan when plan is disable

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 locals {
-  new_plans_list = contains(var.mdc_plans_list,"Databases") ? setsubtract(setunion(var.mdc_plans_list, var.mdc_databases_plans), ["Databases"]) : var.mdc_plans_list
+  final_plans_list = contains(var.mdc_plans_list,"Databases") ? setsubtract(setunion(var.mdc_plans_list, var.mdc_databases_plans), ["Databases"]) : var.mdc_plans_list
 }
 
 resource "azurerm_security_center_subscription_pricing" "asc_plans" {
-  for_each = local.new_plans_list
+  for_each = local.final_plans_list
   tier          = lookup(var.statuses, each.key, var.default_status) ? "Standard" : "Free"
   resource_type = each.value
-  subplan       = lookup(var.subplans, each.key, each.key == "StorageAccounts" ? "PerStorageAccount" : var.default_subplan)
+  subplan       = lookup(var.statuses, each.key, var.default_status) ? lookup(var.subplans, each.key, each.key == "StorageAccounts" ? "PerStorageAccount" : var.default_subplan) : null
 }
