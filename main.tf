0ceab3c4784d350eb591faa975960cfdc4b4da22
oribenartzy <oribenartzy@microsoft.com>
2023-03-05 12:43:58 +0200

    subplan explanation

--- main.tf
+++ main.tf
@@ -10,5 +10,6 @@ resource "azurerm_security_center_subscription_pricing" "asc_plans" {
   for_each = local.final_plans_list
   tier          = lookup(var.statuses, each.key, var.default_status) ? "Standard" : "Free"
   resource_type = each.value
+  # For enable plans, "StorageAccounts" subplan is "PerStorageAccount" and only "VirtualMachines" subplan is configurable. For disable plans, subplan is not required == null.
   subplan       = lookup(var.statuses, each.key, var.default_status) ? lookup(local.updated_subplans, each.key, each.key == "StorageAccounts" ? "PerStorageAccount" : var.default_subplan) : null
 }
