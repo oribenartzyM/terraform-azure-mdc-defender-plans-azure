6f62dcc5fbc1e0fb659e92a03916fdc3987cb364
oribenartzy <oribenartzy@microsoft.com>
2023-03-09 14:53:36 +0200

    Remove comments

--- main.tf
+++ main.tf
@@ -1,8 +1,5 @@
 locals {
   final_plans_list = contains(var.mdc_plans_list, "Databases") ? setsubtract(setunion(var.mdc_plans_list, var.mdc_databases_plans), ["Databases"]) : var.mdc_plans_list
-
-  # Limiting the subplans to "VirtualMachines" only.
-  # updated_subplans = { for k, v in var.subplans : k => v if k == "VirtualMachines" }
 }
 
 resource "azurerm_security_center_subscription_pricing" "asc_plans" {
@@ -10,6 +7,6 @@ resource "azurerm_security_center_subscription_pricing" "asc_plans" {
 
   tier          = lookup(var.statuses, each.key, var.default_status) ? "Standard" : "Free"
   resource_type = each.value
-  # For enable plans, "StorageAccounts" subplan is "PerStorageAccount" and only "VirtualMachines" subplan is configurable. For disable plans, subplan is not required == null.
+  # For enable plans, "StorageAccounts" subplan is "PerStorageAccount". For disable plans, subplan is not required == null.
   subplan = lookup(var.statuses, each.key, var.default_status) ? lookup(var.subplans, each.key, each.key == "StorageAccounts" ? "PerStorageAccount" : var.default_subplan) : null
 }
