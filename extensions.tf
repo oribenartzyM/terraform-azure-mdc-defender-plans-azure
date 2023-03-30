61905a0672a33a59b2e3dd53c13c024d17ada932
oribenartzy <oribenartzy@microsoft.com>
2023-03-30 10:08:43 +0300

    roles

--- extensions.tf
+++ extensions.tf
@@ -1,24 +1,13 @@
 data "azurerm_subscription" "current" {}
 
-# Enabling Endpoint protection
-resource "azurerm_security_center_setting" "setting_mcas" {
-  count = contains(var.mdc_plans_list, "VirtualMachines") ? 1 : 0
 
-  enabled      = true
-  setting_name = "WDATP"
-
-  depends_on = [
-    azurerm_security_center_subscription_pricing.asc_plans["VirtualMachines"]
-  ]
-}
-
+# Enabling vm extensions - Log Analytics for arc and vulnerability assessment
 data "azurerm_policy_definition" "vm_policies" {
   for_each = contains(var.mdc_plans_list, "VirtualMachines") ? local.virtual_machine_policies : {}
 
   display_name = each.value.definition_display_name
 }
 
-# Enabling Policies Log Analytics for arc and vulnerability assessment
 resource "azurerm_subscription_policy_assignment" "vm" {
   for_each = contains(var.mdc_plans_list, "VirtualMachines") ? local.virtual_machine_policies : {}
 
@@ -38,24 +27,7 @@ resource "azurerm_subscription_policy_assignment" "vm" {
   ]
 }
 
-# Enabling Role for Vulnerability assessment
-data "azurerm_role_definition" "security_admin" {
-  name = "Security Admin"
-}
-
-resource "azurerm_role_assignment" "va_auto_provisioning_identity_role" {
-  count = contains(var.mdc_plans_list, "VirtualMachines") ? 1 : 0
-
-  principal_id       = azurerm_subscription_policy_assignment.vm["mdc-va-autoprovisioning"].identity[0].principal_id
-  scope              = data.azurerm_subscription.current.id
-  role_definition_id = data.azurerm_role_definition.security_admin.id
-
-  depends_on = [
-    azurerm_security_center_subscription_pricing.asc_plans["VirtualMachines"]
-  ]
-}
-
-# Enabling Log Analytics for vm
+# Enabling vm extensions - Log Analytics for vm
 resource "azurerm_security_center_auto_provisioning" "auto_provisioning" {
   count = contains(var.mdc_plans_list, "VirtualMachines") ? 1 : 0
 
@@ -66,13 +38,44 @@ resource "azurerm_security_center_auto_provisioning" "auto_provisioning" {
   ]
 }
 
+# Enabling vm extensions - Endpoint protection
+resource "azurerm_security_center_setting" "setting_mcas" {
+  count = contains(var.mdc_plans_list, "VirtualMachines") ? 1 : 0
+
+  enabled      = true
+  setting_name = "WDATP"
+
+  depends_on = [
+    azurerm_security_center_subscription_pricing.asc_plans["VirtualMachines"]
+  ]
+}
+
+# Enabling vm Roles
+data "azurerm_role_definition" "vm_roles" {
+  for_each = contains(var.mdc_plans_list, "VirtualMachines") ? local.virtual_machine_roles : {}
+  
+  name = each.value.name
+}
+
+resource "azurerm_role_assignment" "va_auto_provisioning_vm_role" {
+  for_each = contains(var.mdc_plans_list, "VirtualMachines") ? local.virtual_machine_roles : {}
+
+  principal_id       = azurerm_subscription_policy_assignment.vm[each.value.policy].identity[0].principal_id
+  scope              = data.azurerm_subscription.current.id
+  role_definition_id = data.azurerm_role_definition.vm_roles[each.key].id
+
+  depends_on = [
+    azurerm_security_center_subscription_pricing.asc_plans["VirtualMachines"]
+  ]
+}
+
+# Enabling Containers Extensions - Azure Policy for Kubernetes + Defender DaemonSet
 data "azurerm_policy_definition" "container_policies" {
   for_each = contains(var.mdc_plans_list, "Containers") ? local.container_policies : {}
 
   display_name = each.value.definition_display_name
 }
 
-# Enabling Azure Policy for Kubernetes + Defender DaemonSet
 resource "azurerm_subscription_policy_assignment" "container" {
   for_each = contains(var.mdc_plans_list, "Containers") ? local.container_policies : {}
 
@@ -88,5 +91,24 @@ resource "azurerm_subscription_policy_assignment" "container" {
 
   depends_on = [
     azurerm_security_center_subscription_pricing.asc_plans["Containers"]
+  ]
+}
+
+# Enabling Containers Roles
+data "azurerm_role_definition" "container_roles" {
+  for_each = contains(var.mdc_plans_list, "Containers") ? local.container_roles : {}
+
+  name = each.value.name
+}
+
+resource "azurerm_role_assignment" "va_auto_provisioning_containers_role" {
+  for_each = contains(var.mdc_plans_list, "Containers") ? local.container_roles : {}
+
+  principal_id       = azurerm_subscription_policy_assignment.container[each.value.policy].identity[0].principal_id
+  scope              = data.azurerm_subscription.current.id
+  role_definition_id = data.azurerm_role_definition.container_roles[each.key].id
+
+  depends_on = [
+    azurerm_security_center_subscription_pricing.asc_plans["VirtualMachines"]
   ]
 }
--- locals.tf
+++ locals.tf
@@ -13,6 +13,36 @@ locals {
       definition_display_name = "[Preview]: Configure Azure Arc enabled Kubernetes clusters to install Microsoft Defender for Cloud extension"
     }
   }
+  container_roles = {
+    role-1 = {
+      name = "Kubernetes Extension Contributor"
+      policy = "mdc-containers-kubernetes1-autoprovisioning"
+    }
+    role-2 = {
+      name = "Azure Kubernetes Service Contributor Role"
+      policy = "mdc-containers-kubernetes2-autoprovisioning"
+    }
+    role-3 = {
+      name = "Azure Kubernetes Service Policy Add-on Deployment"
+      policy = "mdc-containers-kubernetes2-autoprovisioning"
+    }
+    role-4 = {
+      name = "Log Analytics Contributor"
+      policy = "mdc-containers_aks_autoprovisioning"
+    }
+    role-5 = {
+      name = "Contributor"
+      policy = "mdc-containers_aks_autoprovisioning"
+    }
+    role-6 = {
+      name = "Log Analytics Contributor"
+      policy = "mdc-containers-arc-autoprovisioning"
+    }
+    role-7 = {
+      name = "Contributor"
+      policy = "mdc-containers-arc-autoprovisioning"
+    }
+  }
   va_type = jsonencode({
     "vaType" = {
       "value" = "mdeTvm"
@@ -27,6 +57,20 @@ locals {
     }
     mdc-log-analytics-arc2-autoprovisioning = {
       definition_display_name = "[Preview]: Configure Azure Arc-enabled Linux machines with Log Analytics agents connected to default Log Analytics workspace"
+    }
+  }
+  virtual_machine_roles = {
+    role-1 = {
+      name = "Security Admin"
+      policy = "mdc-va-autoprovisioning"
+    }
+    role-2 = {
+      name = "Contributor"
+      policy = "mdc-log-analytics-arc1-autoprovisioning"
+    }
+    role-3 = {
+      name = "Contributor"
+      policy = "mdc-log-analytics-arc2-autoprovisioning"
     }
   }
 }
